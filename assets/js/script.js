b21429981d1ae2784f505427cc6bbac78d6fd6f9
Isabelle Moura <isabelle.moura.pb@compasso.com.br>
2024-08-18 14:52:46 -0300

    Carrossel dinamico feito

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,45 @@ function closeSignUpBar(e) {
   closeSignUp.remove();
 }
 
+function carrosselPrevPage() {
+  const carrosselContainer = document.getElementById("section_of_comments");
+  const carrosselContainerLastChild = carrosselContainer.lastElementChild;
+  carrosselContainer.style.transition = "transform 0.5s ease-in";
+  carrosselContainer.style.transform = `translateX(${carrosselContainerLastChild.clientWidth}px)`;
+  carrosselContainer.addEventListener(
+    "transitionend",
+    () => {
+      carrosselContainer.removeChild(carrosselContainerLastChild);
+      carrosselContainer.insertBefore(
+        carrosselContainerLastChild,
+        carrosselContainer.firstElementChild
+      );
+
+      carrosselContainer.style.transition = "none";
+      carrosselContainer.style.transform = "translateX(0)";
+    },
+    { once: true }
+  );
+}
+
+function carrosselNextPage() {
+  const carrosselContainer = document.getElementById("section_of_comments");
+  const carrosselContainerFirstChild = carrosselContainer.firstElementChild;
+  carrosselContainer.style.transition = "transform 0.5s ease-in";
+  carrosselContainer.style.transform = `translateX(-${carrosselContainerFirstChild.clientWidth}px)`;
+  carrosselContainer.addEventListener(
+    "transitionend",
+    () => {
+      carrosselContainer.removeChild(carrosselContainerFirstChild);
+      carrosselContainer.appendChild(carrosselContainerFirstChild);
+
+      carrosselContainer.style.transition = "none";
+      carrosselContainer.style.transform = "translateX(0)";
+    },
+    { once: true }
+  );
+}
+
 function subscribeToNewsletter(e) {
   e.preventDefault();
 
@@ -30,11 +69,17 @@ function subscribeToNewsletter(e) {
 }
 
 function init() {
-  const subscribeToNewsletterForm = document.getElementById("newsletter-form");
-  subscribeToNewsletterForm.addEventListener("submit", subscribeToNewsletter);
-
   const closeXBar = document.getElementById("x_close_button");
   closeXBar.addEventListener("click", closeSignUpBar);
+
+  const prevPage = document.getElementById("arrow-left");
+  prevPage.addEventListener("click", carrosselPrevPage);
+
+  const nextPage = document.getElementById("arrow-right");
+  nextPage.addEventListener("click", carrosselNextPage);
+
+  const subscribeToNewsletterForm = document.getElementById("newsletter-form");
+  subscribeToNewsletterForm.addEventListener("submit", subscribeToNewsletter);
 }
 
 init();
